2843cb43305b1223a0b7f6cc1ee6952094abf9a1
Kasztan <kasztan1015@wp.pl>
2022-10-03 18:20:57 +0200

    Added files from task A and B

--- WRO_JEE_S_12_Prework_Java-master/src/main/java/pl/coderslab/ckontrolaprzeplywu/Main01.java
+++ WRO_JEE_S_12_Prework_Java-master/src/main/java/pl/coderslab/ckontrolaprzeplywu/Main01.java
@@ -3,6 +3,16 @@ package pl.coderslab.ckontrolaprzeplywu;
 public class Main01 {
 
     public static void main(String[] args) {
+        int nr1 = 3;
+        int nr2 = 7;
+        boolean nr3 = (nr1 > nr2);
+        if (nr3 == true) {
+            System.out.println(nr1);
+        } else {
+            System.out.println(nr2);
+        }
+
+
 
     }
 
--- WRO_JEE_S_12_Prework_Java-master/src/main/java/pl/coderslab/ckontrolaprzeplywu/Main02.java
+++ WRO_JEE_S_12_Prework_Java-master/src/main/java/pl/coderslab/ckontrolaprzeplywu/Main02.java
@@ -3,6 +3,14 @@ package pl.coderslab.ckontrolaprzeplywu;
 public class Main02 {
 
 	public static void main(String[] args) {
+		int nr1 = 3;
+		int nr2 = 7;
+		int nr3 = 11;
+		boolean
+
+
+
+		}
 
 	}
 
