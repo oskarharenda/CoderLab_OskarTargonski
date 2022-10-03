4f3c0d462fe6bdd2c82d46a3d6e77a7a5b05227d
Kasztan <kasztan1015@wp.pl>
2022-10-04 00:43:43 +0200

    Wszystkie taskie z 3 pierwszych blokow

--- WRO_JEE_S_12_Prework_Java-master/src/main/java/pl/coderslab/ckontrolaprzeplywu/Main02.java
+++ WRO_JEE_S_12_Prework_Java-master/src/main/java/pl/coderslab/ckontrolaprzeplywu/Main02.java
@@ -6,10 +6,13 @@ public class Main02 {
 		int nr1 = 3;
 		int nr2 = 7;
 		int nr3 = 11;
-		boolean
 
-
-
+		if (nr1 > nr2 && nr1 > nr3) {
+			System.out.print("Najwięskza liczba to " + nr1);
+		} else if (nr2 > nr1 && nr2 > nr3) {
+			System.out.print("Najwięskza liczba to " + nr2);
+		} else {
+			System.out.print("Najwięskza liczba to " + nr3);
 		}
 
 	}
--- WRO_JEE_S_12_Prework_Java-master/src/main/java/pl/coderslab/ckontrolaprzeplywu/Main04.java
+++ WRO_JEE_S_12_Prework_Java-master/src/main/java/pl/coderslab/ckontrolaprzeplywu/Main04.java
@@ -3,6 +3,15 @@ package pl.coderslab.ckontrolaprzeplywu;
 public class Main04 {
 
 	public static void main(String[] args) {
+		for (int i = 1; i <=10; i = i + 1) {
+			System.out.print(i + " ");
+		}
+			System.out.print("\n");
+		int i = 1;
+		while (i <=10) {
+			System.out.print(i + " ");
+			i++;
+		}
 
 	}
 
