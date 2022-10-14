5ff245ab3ff1296cf31436519d86690c69c176ed
Kasztan <kasztan1015@wp.pl>
2022-10-14 21:53:59 +0200

    zadania dzien 4

--- WRO_JEE_S_12_Podstawy-master/a_Zadania/d_Dzien_4/b_Pliki/Main3.java
+++ WRO_JEE_S_12_Podstawy-master/a_Zadania/d_Dzien_4/b_Pliki/Main3.java
@@ -1,10 +1,36 @@
 package a_Zadania.d_Dzien_4.b_Pliki;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static sun.util.locale.LocaleUtils.isNumeric;
+
 public class Main3 {
 
     public static void main(String[] args) {
-
-
+        File file = new File("a_Zadania/d_Dzien_4/b_Pliki/text1.txt");
+        StringBuilder text = new StringBuilder();
+        try {
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                text.append(scan.nextLine() + "\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Błąd pliku");
+            System.out.println(e);
+        }
+        String test = text.toString();
+        System.out.println(test);
+        String[] table = test.split(",");
+        int result = 0;
+        int sum = 0;
+        for (String s : table) {
+            if (isNumeric(s)){
+                result += sum;
+            }
+        }
+        System.out.println(result);
     }
 }
