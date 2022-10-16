ad4d8260aba84a53e14c0e8a919c20cb9c836ec0
Kasztan <kasztan1015@wp.pl>
2022-10-16 19:38:33 +0200

    Zadania domowe dzien 1

--- WRO_JEE_S_12_Podstawy-master/b_Zadania_Domowe/a_Dzien_1/Main2.java
+++ WRO_JEE_S_12_Podstawy-master/b_Zadania_Domowe/a_Dzien_1/Main2.java
@@ -4,8 +4,20 @@ package b_Zadania_Domowe.a_Dzien_1;
 public class Main2 {
 
     public static void main(String[] args) {
+        boolean check = divisibleBy(28, 3);
+        System.out.print(check);
 
+    }
 
+    static boolean divisibleBy(int a, int b) {
+        boolean checker;
+        if (a % b == 0) {
+            checker = true;
+
+        } else {
+            checker = false;
+        }
+        return checker;
     }
 
 }
